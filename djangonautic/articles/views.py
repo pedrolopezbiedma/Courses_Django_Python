7c82d14b8a5b55e857fd8c3428b19c941ebff0da
Pedro Lopez <Pedro.Lopez@ngahr.com>
2021-08-25 09:05:00 +0200

    Including changes until Lesson 13

--- djangonautic/articles/views.py
+++ djangonautic/articles/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from .models import Article
 
 def articles_list(request): 
-    return render(request, 'articles/articles_list.html')
+    articles_list = Article.objects.all().order_by('date')
+    return render(request, 'articles/articles_list.html', {'articles_list': articles_list})
--- djangonautic/djangonautic/urls.py
+++ djangonautic/djangonautic/urls.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 from django.urls import path
 from django.urls.conf import include
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -9,3 +11,5 @@ urlpatterns = [
     path('articles/', include('articles.urls')),
     path('about/', views.about )
 ]
+
+urlpatterns += static(settings.STATIC_URL)
