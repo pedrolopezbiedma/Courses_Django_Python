b80a9027eb0a03c8e7b9fb7076ad9f4a4c6ea3fb
Pedro Lopez <Pedro.Lopez@ngahr.com>
2021-08-30 09:10:13 +0200

    Continue the courses, lesson 17

--- djangonautic/articles/urls.py
+++ djangonautic/articles/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
 
+app_name='articles'
+
 urlpatterns = [
-    path('', views.articles_list )
+    path('', views.articles_list, name='articles_list' ),
+    path('<slug:slug>/', views.article_details, name='article_details')
 ]
--- djangonautic/articles/views.py
+++ djangonautic/articles/views.py
@@ -1,6 +1,11 @@
+from django.http.response import HttpResponse
 from django.shortcuts import render
 from .models import Article
 
 def articles_list(request): 
     articles_list = Article.objects.all().order_by('date')
     return render(request, 'articles/articles_list.html', {'articles_list': articles_list})
+
+def article_details(request, slug): 
+    article = Article.objects.get(slug=slug)
+    return render(request, 'articles/article_details.html', { 'article': article })
